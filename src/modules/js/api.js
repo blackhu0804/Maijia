d35699c25354c88fcf5d4707ccd773751bf78341
hu970804 <812510003@qq.com>
2018-05-16 23:43:28 +0800

    修改为多页面应用，添加首页静态资源和axios请求数据

---
+++ src/modules/js/api.js
@@ -0,0 +1,15 @@
+let url = {
+    hotLists :'/index/hotLists',
+}
+
+//  开发环境和真是环境的切换
+// let host = ''
+let host = 'http://rap2api.taobao.org/app/mock/7058'
+
+for(let key in url) {
+    if(url.hasOwnProperty(key)) {
+        url[key] = host + url[key]
+    }
+}
+
+export default url
