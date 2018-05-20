18e02fb656625df928f74accb8925d7b73651373
hu970804 <812510003@qq.com>
2018-05-20 12:02:54 +0800

    add money filters

--- src/pages/category/category.js
+++ src/pages/category/category.js
@@ -46,5 +46,25 @@ new Vue({
     },
     components: {
         Foot
+    },
+    filters: {
+        money(val) {
+            val = val.toString().replace(/\$|\,/g, '');
+            if (isNaN(val)) {
+                val = "0";
+            }
+            let sign = (val == (val = Math.abs(val)));
+            val = Math.floor(val * 100 + 0.50000000001);
+            let cents = val % 100;
+            val = Math.floor(val / 100).toString();
+            if (cents < 10) {
+                cents = "0" + cents
+            }
+            for (var i = 0; i < Math.floor((val.length - (1 + i)) / 3); i++) {
+                val = val.substring(0, val.length - (4 * i + 3)) + ',' + val.substring(val.length - (4 * i + 3));
+            }
+
+            return (((sign) ? '' : '-') + val + '.' + cents);
+        }
     }
 })
