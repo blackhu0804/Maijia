569b41c8c2a90ae02d81115b62467792b42a9a80
hu970804 <812510003@qq.com>
2018-05-21 13:38:36 +0800

    update foot component

--- src/components/Foot.vue
+++ src/components/Foot.vue
@@ -1,28 +1,14 @@
 <template>
   <div class="bottom-nav">
     <ul>
-      <li class="active">
-        <a href="index.html">
-          <i class="icon-home"></i>
-          <div>有赞</div>
-        </a>
-      </li>
-      <li>
-        <a href="category.html">
-          <i class="icon-category"></i>
-          <div>分类</div>
-        </a>
-      </li>
-      <li>
-        <a href="cart.html">
-          <i class="icon-cart"></i>
-          <div>购物车</div>
-        </a>
-      </li>
-      <li>
-        <a href="member.html">
-          <i class="icon-user"></i>
-          <div>我</div>
+      <li :class="{active: index == curIndex}" 
+        v-for="(list, index) in navConfig"
+        @click="changeNav(list, index)"
+        :key="list.id"
+      >
+        <a>
+          <i :class="list.icon"></i>
+          <div>{{list.name}}</div>
         </a>
       </li>
     </ul>
@@ -31,8 +17,41 @@
 
 
 <script>
-  export default {
+  import qs from 'qs'
+  let {index} = qs.parse(location.search.substr(1))
 
+  let navConfig = [{
+    name: '首页',
+    icon: 'icon-home',
+    href: 'index.html'
+  },{
+    name: '分类',
+    icon: 'icon-category',
+    href: 'category.html'
+  },{
+    name: '购物车',
+    icon: 'icon-cart',
+    href: 'cart.html'
+  },{
+    name: '我',
+    icon: 'icon-user',
+    href: 'member.html'
+  }]
+
+  export default {
+    data() {
+      return {
+        navConfig,
+        curIndex: parseInt(index) || 0
+      }
+    },
+    methods: {
+      changeNav(list, index) {
+        location.href = `${list.href}?index=${index}`
+        console.log(`list.href: ${list.href}`)
+        console.log(`index: ${index}`)
+      }
+    }
   }
 
 </script>
