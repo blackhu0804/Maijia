4930cb2bb66ec2151199897bfa43b0c29cc4b962
hu970804 <812510003@qq.com>
2018-05-21 15:35:08 +0800

    添加三个入口跳转good.html

---
+++ src/pages/goods/goods.js
@@ -0,0 +1,18 @@
+import './goods_common.css'
+import './goods_custom.css'
+import './goods.css'
+import './goods_theme.css'
+import './goods_mars.css'
+import './goods_sku.css'
+import './goods_transition.css'
+
+import Vue from 'vue'
+import url from 'js/api.js'
+import axios from 'axios'
+import mixin from 'js/mixin.js'
+import qs from 'qs'
+
+new Vue({
+  el: '#app',
+})
+
