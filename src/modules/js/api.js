4109cde31799d661764b0134c8b1e1dd5582bd48
hu970804 <812510003@qq.com>
2018-06-18 12:16:06 +0800

    完成购物车列表渲染和商品选中取消

--- src/modules/js/api.js
+++ src/modules/js/api.js
@@ -7,7 +7,11 @@ let url = {
     searchList :'/search/list',
     details :'/goods/details',
     deal : '/goods/deal',
-    addCart : '/cart/add'
+    addCart : '/cart/add',
+    cartLists: '/cart/list',
+    cartReduce: '/cart/reduce',
+    cartRemove: '/cart/remove',
+    cartMremove: '/cart/mrremove'
 }
 
 //  开发环境和真是环境的切换
